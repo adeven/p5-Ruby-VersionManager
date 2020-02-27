fa423ca76c549620619400d30ca7af44d74fc408
Wiktor Kerr <wiktor@adjust.com>
2020-02-27 16:10:22 +0000

    add ruby 2.7

--- lib/Ruby/VersionManager.pm
+++ lib/Ruby/VersionManager.pm
@@ -96,7 +96,7 @@ sub _check_installed {
 sub updatedb {
     my ($self) = @_;
 
-    my @versions = qw/1.8 1.9 2.0 2.1 2.2 2.3 2.4/;
+    my @versions = qw/1.8 1.9 2.0 2.1 2.2 2.3 2.4 2.7/;
 
     my $rubies = {};
 
